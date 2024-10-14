c083a0f908560cf3b75b1330cd128dab44d26dcd
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-10-15 00:14:42 +0300

    updated Routes

--- EXPRESSJS_TODO/routes.js
+++ EXPRESSJS_TODO/routes.js
@@ -1,22 +1,40 @@
 const express = require('express');
-const User = require('./models')
+const Task = require('./models')
 
 const router = express.Router();
 
 router.get('/todo', async(request, response) => {
-    const tasks = await User.findAll();
+    const tasks = await Task.findAll();
 
     response.status(200).json(tasks);
 })
 
-router.post('/todo', (request, response) => {
-    const data = request.body;
+router.post('/todo', async(request, response) => {
+    const { content, description } = request.body;
 
-    response.status(201).json(data);
+    const newTask = Task.build({
+        'content':content,
+        'description':description
+    })
+    
+    try {
+        await newTask.save()
+        response.status(201).json(newTask);
+        
+    } catch (error) {
+       response.json(error) 
+    }
 })
 
 
-router.get('/todo/:id', (request, response) => {
+router.get('/todo/:id', async(request, response) => {
+    const task = await Task.findOne({
+        where: {
+            id:request.params.id
+        }
+    })
+
+    response.status(200).json(task)
 
 })
 
